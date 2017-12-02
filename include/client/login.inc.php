5b361f9525b80bda881050a93be453891662a21b
Administrador <Administrador@TritiumPC.aym.juntaex.pri>
2017-12-02 13:06:06 +0100

    Se simplifica la interfaz al eliminar un mensaje de la ventana de login de usuarios que sugería la apertura de un ticket si se contactaba por primera vez o se perdía el número de ticket. Sólo se elimina en caso de que el sistema esté configurado para requerir estar registrado como usuario.

--- include/client/login.inc.php
+++ include/client/login.inc.php
@@ -64,8 +64,8 @@ if ($cfg && $cfg->isClientRegistrationEnabled()) {
 <br>
 <p>
 <?php
-if ($cfg->getClientRegistrationMode() != 'disabled'
-    || !$cfg->isClientLoginRequired()) {
+if (/*$cfg->getClientRegistrationMode() != 'disabled'   // No me parece que esta condición determine si se pone el mensaje o no
+    || */!$cfg->isClientLoginRequired()) {
     echo sprintf(__('If this is your first time contacting us or you\'ve lost the ticket number, please %s open a new ticket %s'),
         '<a href="open.php">', '</a>');
 } ?>
