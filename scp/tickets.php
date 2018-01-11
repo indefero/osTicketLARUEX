b81e3351f33b828a10e8e43fcc0f0eebae50353f
Administrador <Administrador@TritiumPC.aym.juntaex.pri>
2018-01-11 20:12:43 +0100

    Se corrige el problema de filtrado de tickets en exportación a formato CSV. Si se realizaban exportaciones consecutivas sin salir de la página el filtrado se iba concatenando en lugar de ser independientes. Esto se debe a que se llama a un método de exportación (de la clase export) con el objeto QuerySet como parámetro. Al ser un objeto se pasa por referencia y todas las modificaciones se están traspasando al mismo objeto almacenado en la sesión del navegador, de manera que la siguiente llamada parte de un conjunto de tickets ya filtrado. Se corrige simplemente llamando al método de exportación con una clonación del objeto QuerySet de los tickets en lugar del original que se guarda en la sesión.

--- scp/tickets.php
+++ scp/tickets.php
@@ -484,7 +484,7 @@ if($ticket) {
         $inc = 'ticket-open.inc.php';
     elseif($_REQUEST['a'] == 'export') {
         $ts = strftime('%Y%m%d');
-        if (!($query=$_SESSION[':Q:tickets']))
+        if (!($query=clone $_SESSION[':Q:tickets']))
             $errors['err'] = __('Query token not found');
         elseif (!Export::saveTickets($query, "tickets-$ts.csv", 'csv', $_REQUEST['tids'] ? array_filter(explode(',', $_REQUEST['tids'])) : null))
             $errors['err'] = __('Unable to dump query results.')
